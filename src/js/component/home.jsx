ee61c517538dfdfb60fdca88d66be850bed54df8
Radovan-geek <131044960+Radovan-geek@users.noreply.github.com>
2023-07-07 03:12:48 +0000

    To do list assignment

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
+//cuando usar el use state?
 //create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = useState("");
+	const [todos, setTodos] = useState([]);
+	const handleImputCambio = (event) => {
+		setInputValue(event.target.value)
+	}
+	const handleEnter = (event) => {
+		if (event.key === "Enter") {
+			agregar();
+		}
+	}
+	const agregar = () => {
+		if (inputValue.trim() !== "") {
+			setTodos([...todos, inputValue]);
+			setInputValue("")
+		}
+	}
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1>MY TASKS</h1>
+			<ul>
+
+				<li>
+					<input
+						type="text"
+						onChange={handleImputCambio}
+						value={inputValue}
+						onKeyPress={handleEnter}
+						placeholder="What stuff do you need to do?">
+					</input>
+				</li>
+				{
+					todos.length == 0 ? (
+						<li>there is no task to add</li>
+					) : (
+						todos.map((item, index) => {
+							return (
+								<li className="hover">
+									{item}{""}
+									<i class="fa-solid fa-trash cesto"
+										onClick={() =>
+											setTodos(
+												todos.filter(
+													(t, currentIndex) =>
+														index != currentIndex
+												)
+											)
+										}></i>
+								</li>
+							)
+						})
+					)
+				}
+				<div className='card m-1 container-fluid counter'>{todos.length} pending items</div>
+			</ul>
+
+			{/* <div>23 task</div> */}
 		</div>
 	);
 };
-
 export default Home;
